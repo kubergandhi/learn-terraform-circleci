47807325887b203b357d4f8f59ab955dd8896158
kubergandhi <138006454+kubergandhi@users.noreply.github.com>
2023-09-18 10:15:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,25 +20,6 @@ resource "aws_s3_bucket" "app" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = aws_s3_bucket.app.id
-
-  policy = <<EOF
-{
-  "Version":"2012-10-17",
-  "Statement":[
-    {
-      "Sid":"PublicReadGetObject",
-      "Effect":"Allow",
-      "Principal": "*",
-      "Action":["s3:GetObject"],
-      "Resource":["arn:aws:s3:::${aws_s3_bucket.app.id}/*"]
-    }
-  ]
-}
-EOF
-}
-
 resource "aws_s3_object" "app" {
   key          = "index.html"
   bucket       = aws_s3_bucket.app.id
