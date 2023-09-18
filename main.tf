4e4831c5e552c43719a43349239a05b4000757f3
kubergandhi <138006454+kubergandhi@users.noreply.github.com>
2023-09-18 11:08:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,18 +22,20 @@ resource "aws_s3_bucket" "app" {
 resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.app.id
 
-  policy = <<EOF
-{
-  "Version":"2012-10-17",
-  "Statement":[
-    {
-      "Sid":"PublicReadGetObject",
-      "Effect":"Allow",
-      "Principal": "*",
-      "Action":["s3:GetObject"],
-      "Resource":["arn:aws:s3:::${aws_s3_bucket.app.id}/*"]
-    }
-  ]
+  policy = resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.app.id
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": "s3:GetObject",
+        "Resource": "arn:aws:s3:::${aws_s3_bucket.app.id}/*"
+      }
+    ]
+  })
 }
 EOF
 }
